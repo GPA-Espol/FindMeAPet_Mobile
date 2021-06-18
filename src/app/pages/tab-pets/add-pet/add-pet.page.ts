f7d45f23a609c0aaa33f2d012b1b582386d8f627
Valeria Barzola <valeria.barzola@espol.edu.ec>
2021-06-17 22:09:16 -0500

    minor changes

--- src/app/pages/tab-pets/add-pet/add-pet.page.ts
+++ src/app/pages/tab-pets/add-pet/add-pet.page.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPetPage implements OnInit {
   extraInformation: boolean = false;
-
+  ageType: string = '';
   constructor() { }
 
   ngOnInit() {
