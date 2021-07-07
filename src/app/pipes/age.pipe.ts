efbad605212182d9d6c9e86d2fb176a4c5d9e949
AlexVelezLl <alanvele@espol.edu.ec>
2021-07-06 22:40:53 -0500

    Testing pipes

--- src/app/pipes/age.pipe.ts
+++ src/app/pipes/age.pipe.ts
@@ -14,16 +14,15 @@ export class AgePipe implements PipeTransform {
   transform(d: any): string {
     let currentDate = new Date(new Date().toUTCString());
     let date = new Date(d + 'Z');
-
+    let offsetTimeZoneHours = date.getTimezoneOffset() / 60;
+    date.setHours(date.getHours() + offsetTimeZoneHours);
     let year = currentDate.getFullYear() - date.getFullYear();
     let month = currentDate.getMonth() - date.getMonth();
     let day = currentDate.getDate() - date.getDate();
     let hour = currentDate.getHours() - date.getHours();
     let minute = currentDate.getMinutes() - date.getMinutes();
     let second = currentDate.getSeconds() - date.getSeconds();
-
     let createdSecond = year * 31556926 + month * 2629746 + day * 86400 + hour * 3600 + minute * 60 + second;
-
     if (createdSecond >= 31556926) {
       let yearAgo = Math.floor(createdSecond / 31556926);
       return yearAgo > 1 ? yearAgo + ' años' : yearAgo + ' año';
@@ -32,6 +31,7 @@ export class AgePipe implements PipeTransform {
       return monthAgo > 1 ? monthAgo + ' meses' : monthAgo + ' mes';
     } else if (createdSecond >= 86400) {
       let dayAgo = Math.floor(createdSecond / 86400);
+      console.log(dayAgo);
       return dayAgo > 1 ? dayAgo + ' días' : dayAgo + ' día';
     }
   }
