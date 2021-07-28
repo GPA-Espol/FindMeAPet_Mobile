bb3a4239e17d53b336f9a81b18fef46f25e304d6
AlexVelezLl <alanvele@espol.edu.ec>
2021-07-28 18:26:07 -0500

    Add EmtpyList component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AdminCardComponent } from './admin-card/admin-card.component';
 import { IonicModule } from '@ionic/angular';
 import { ManageCardComponent } from './manage-card/manage-card.component';
+import { EmptyListComponent } from './empty-list/empty-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AdminCardComponent, ManageCardComponent],
-  imports: [CommonModule, IonicModule],
-  exports: [AdminCardComponent, ManageCardComponent],
+  declarations: [AdminCardComponent, ManageCardComponent, EmptyListComponent],
+  imports: [CommonModule, IonicModule, RouterModule],
+  exports: [AdminCardComponent, ManageCardComponent, EmptyListComponent],
 })
 export class ComponentsModule {}
